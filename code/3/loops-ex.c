e187a8ca40a090ef5fcbddc4d3bc3e9db60c2956
Neel Mehta <neelmehta@college.harvard.edu>
2015-09-22 17:36:03 -0400

    Add loops example code

---
+++ code/3/loops-ex.c
@@ -0,0 +1,14 @@
+/**
+ * Prints every even number between 0 and 10, inclusive.
+ */
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%i\n", i);
+    }
+}
