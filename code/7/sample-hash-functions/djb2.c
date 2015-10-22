5c1d8a5a293b2fdb353568347ec4459604d6da3e
Neel Mehta <neelmehta@college.harvard.edu>
2015-10-22 00:01:32 -0400

    Add djb2 sample hash function

---
+++ code/7/sample-hash-functions/djb2.c
@@ -0,0 +1,28 @@
+/*
+ * The one hash function that'll actually, you know, work!
+ * Feel free to use this function, just provide attribution
+ * in a comment!
+ *
+ * Adapted by Neel Mehta from
+ * http://stackoverflow.com/questions/2571683/djb2-hash-function
+ *
+ */
+
+/**
+ * A case-insensitive implementation of the djb2 hash function.
+ * Change NUM_BUCKETS to whatever your number of buckets is.
+ *
+ * Adapted by Neel Mehta from
+ * http://stackoverflow.com/questions/2571683/djb2-hash-function.
+ */
+unsigned int hash_word(const char* word)
+ {
+     unsigned long hash = 5381;
+
+     for (const char* ptr = word; *ptr != '\0'; ptr++)
+     {
+         hash = ((hash << 5) + hash) + tolower(*ptr);
+     }
+
+     return hash % NUM_BUCKETS;
+ }
